98379b91a269f4f2dfc86fe99dfc04e4c64522ae
龙骑将杨影枫 <catwindboy@hotmail.com>
2022-04-07 23:28:49 +0800

    feat: 法环刷魂

--- index.js
+++ index.js
@@ -2,17 +2,57 @@ var robot = require("robotjs");
 
 // Type "Hello World".
 
-const keys = ['space', 'f1', 'a', ]
+const keys = [ 'q', 'v','v','v'
+
+// ,'v','v','v','v','v','v','v'
+,'e' ]
 let index = 0;
-const timer = setInterval(()=> {
-    const keyIndex = Math.floor(Math.random() * keys.length);
-    console.log(keyIndex);
-    const keyCode = keys[keyIndex];
-    console.log(keyCode);
-    robot.keyTap(keyCode);
-    console.log(`触发${index}`)
-    if(index === 1000) {
-        clearInterval(timer);
+const delay = (time = 1000) => new Promise((res) => {
+    setTimeout(() => {
+        res()
+    }, time)
+})
+// const timer = setInterval(async ()=> {
+//     let index;
+//     const current = new Date().getTime()
+//     for(index = 0;index < keys.length; index ++) {
+//         console.log(index);
+//         const keyCode = keys[index];
+//         console.log(keyCode);
+//         robot.keyTap(keyCode);
+//         console.log(`触发${index}`)
+//         await delay(2500)
+//     }
+//     console.log(new Date().getTime() - current)
+// }, 40000)
+
+const test = async () => {
+    let index;
+    const current = new Date().getTime()
+    for(index = 0;index < keys.length; index ++) {
+        await delay(1500)
+        console.log(index);
+        const keyCode = keys[index];
+        console.log(keyCode);
+        // robot.setKeyboardDelay(300);
+        robot.typeString(keyCode);
+        // robot.keyToggle(keyCode, 'down');
+        // robot.keyToggle(keyCode, 'down');
+        // robot.keyToggle(keyCode, 'down');
+        // robot.keyToggle(keyCode, 'down');
+        // robot.keyToggle(keyCode, 'down');
+        // robot.keyToggle(keyCode, 'down');
+        // await delay(500)
+        // robot.keyTap(keyCode);
+        await delay(100)
+        // robot.keyToggle(keyCode, 'up');
+        // await delay(500)
+        
+        // robot.keyToggle(keyCode, 'up');
+        console.log(`触发${index}`)
+        await delay(500)
     }
-    index ++;
-}, 10000)
+    console.log(new Date().getTime() - current)
+}
+
+test()
